bdaf7a48a8c280f8c50757543c2e8dd3e035ea8e
jiyabhowmick-collab <jiyabhowmick39@gmail.com>
2026-02-22 13:52:18 +0530

    Add student grade calculation program in C

---
+++ result.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+float calculatePercentage(int total)
+{
+    return total / 3.0;
+}
+
+char calculateGrade(float percentage)
+{
+    if (percentage >= 90)
+        return 'A';
+    else if (percentage >= 75)
+        return 'B';
+    else if (percentage >= 60)
+        return 'C';
+    else if (percentage >= 40)
+        return 'D';
+    else
+        return 'F';
+}
+
+int main()
+{
+    char name[50];
+    int m1, m2, m3, total;
+    float percentage;
+    char grade;
+
+    printf("Enter Student Name: ");
+    scanf("%s", name);
+
+    printf("Enter marks of 3 subjects: ");
+    scanf("%d %d %d", &m1, &m2, &m3);
+
+    total = m1 + m2 + m3;
+
+    percentage = calculatePercentage(total);
+
+    grade = calculateGrade(percentage);
+
+    printf("\n--- Result ---\n");
+    printf("Name: %s\n", name);
+    printf("Total Marks: %d\n", total);
+    printf("Percentage: %.2f\n", percentage);
+    printf("Grade: %c\n", grade);
+
+    return 0;
+}
